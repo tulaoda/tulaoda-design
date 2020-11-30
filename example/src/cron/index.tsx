c75f780bab8818f8eb6a6921e0920be54786d2d9
tulaoda <coderaxin@163.com>
2020-11-30 11:19:10 +0800

    feat(rc): feat add CronParse

--- example/src/cron/index.tsx
+++ example/src/cron/index.tsx
@@ -3,7 +3,7 @@ import * as React from 'react'
 import CronEditor from 'components/cron-editor'
 
 const Cron = (): React.ReactElement => {
-    return <CronEditor />
+    return <CronEditor showRunTime showCrontab />
 }
 
 export default Cron
